641a13630a98c9403cf4558417b65411b01132fc
hadi <work.hadimohammadi@gmail.com>
2025-05-06 15:27:38 +0330

    feature: added hilt dependencies to poet-list feature

--- features/poet-list/build.gradle.kts
+++ features/poet-list/build.gradle.kts
@@ -40,4 +40,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.dagger.hilt)
+    implementation(libs.dagger.hilt.compiler)
+    implementation(libs.dagger.hilt.navigation)
 }
